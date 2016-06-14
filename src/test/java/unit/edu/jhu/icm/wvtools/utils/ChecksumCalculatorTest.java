c395bbe15de08b37e69e81b6f86c9a584e731e33
Absox <xabsox@gmail.com>
2016-06-14 17:34:35 -0400

    Unit tested checksum calculator.

--- src/test/java/unit/edu/jhu/icm/wvtools/utils/ChecksumCalculatorTest.java
+++ src/test/java/unit/edu/jhu/icm/wvtools/utils/ChecksumCalculatorTest.java
@@ -1,7 +1,38 @@
 package unit.edu.jhu.icm.wvtools.utils;
 
+import edu.jhu.icm.wvtools.util.ChecksumCalculator;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
 /**
+ * Unit tests for checksum calculator.
  * Created by ran on 6/14/16.
  */
 public class ChecksumCalculatorTest {
+
+    private ChecksumCalculator calculator;
+
+    @Before
+    public void initialize() {
+        calculator = new ChecksumCalculator(2);
+    }
+
+    @Test
+    public void testAdditions() {
+        calculator.addShort((short)32767, 0);
+        assertTrue(32767 == calculator.getShort(0));
+        calculator.addShort((short)1, 0);
+        assertTrue(-32768 == calculator.getShort(0));
+    }
+
+    @Test
+    public void testMultiChannel() {
+        calculator.addInt(5, 0);
+        calculator.addInt(2, 1);
+        assertTrue(5 == calculator.getInt(0));
+        assertTrue(2 == calculator.getInt(1));
+    }
+
 }
